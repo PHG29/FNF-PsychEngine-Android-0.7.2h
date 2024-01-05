f5219b419d034e0da93136b5308924a5c9caadf4
Pedro Henrique <97354032+PHG29@users.noreply.github.com>
2024-01-05 17:49:45 -0300

    Update FunkinLua.hx

--- source/psychlua/FunkinLua.hx
+++ source/psychlua/FunkinLua.hx
@@ -40,6 +40,9 @@ import flixel.input.gamepad.FlxGamepadInputID;
 
 import haxe.Json;
 
+//Custom New
+import flxgif.FlxGifSprite;
+
 class FunkinLua {
 	public static var Function_Stop:Dynamic = "##PSYCHLUA_FUNCTIONSTOP";
 	public static var Function_Continue:Dynamic = "##PSYCHLUA_FUNCTIONCONTINUE";
